a7bb07d75049f6a8e6f91efd69d1404df171b2cf
Zahar Domolego <domolegozahar@gmail.com>
2022-02-02 00:35:36 +0800

    Добавил анимацию на изображение в стартовом фрагменте

--- app/src/main/java/com/example/studassistant/fragments/StartFragment.java
+++ app/src/main/java/com/example/studassistant/fragments/StartFragment.java
@@ -1,9 +1,11 @@
 package com.example.studassistant.fragments;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,11 +13,18 @@ import androidx.fragment.app.Fragment;
 
 import com.example.studassistant.R;
 
-public class StartFragment extends Fragment {
+public class StartFragment extends Fragment{
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_start, container, false);
+        View view = inflater.inflate(R.layout.activity_start, container, false);
+
+        ImageView startImage = view.findViewById(R.id.startImage);
+        startImage.setBackgroundResource(R.drawable.start_animation);
+        AnimationDrawable animationDrawable = (AnimationDrawable) startImage.getBackground();
+        animationDrawable.start();
+
+        return view;
     }
 }
