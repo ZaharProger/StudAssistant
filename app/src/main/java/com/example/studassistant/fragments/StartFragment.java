f9a9b5a7e33b3d5995f3e0c984cb50fbfd304354
Zahar Domolego <domolegozahar@gmail.com>
2022-03-21 23:30:45 +0800

    Вернул анимацию на главный экран

--- app/src/main/java/com/example/studassistant/fragments/StartFragment.java
+++ app/src/main/java/com/example/studassistant/fragments/StartFragment.java
@@ -1,9 +1,11 @@
 package com.example.studassistant.fragments;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,7 +20,11 @@ public class StartFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_start, container, false);
 
+        ImageView startImage = view.findViewById(R.id.startImage);
 
+        startImage.setBackgroundResource(R.drawable.start_animation);
+        AnimationDrawable animationDrawable = (AnimationDrawable) startImage.getBackground();
+        animationDrawable.start();
 
         return view;
     }
